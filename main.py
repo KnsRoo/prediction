2b2b5c9579606670bc04ab4d6430f88acf04277a
KnsRoo <knsroo@gmail.com>
2022-05-25 00:15:05 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys, os
+from PyQt5 import QtCore, QtWidgets, QtGui
+from lib.window import Window
+
+LOG_QT_WARNINGS = True
+
+if not LOG_QT_WARNINGS:
+	QtCore.qInstallMessageHandler(lambda x,y,z: None)
+
+if __name__ == "__main__":
+	app = QtWidgets.QApplication(sys.argv)
+	mainWin = Window()
+	mainWin.show()
+	sys.exit(app.exec_())
